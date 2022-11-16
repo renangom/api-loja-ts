a4ffcfcd2e2621a714c517a5f004c5ed4a7bcd07
Renan <renan23eduardo@gmail.com>
2022-11-16 00:32:58 -0300

    Create createCategory use case

--- src/app/useCases/Category/createCategory.ts
+++ src/app/useCases/Category/createCategory.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(req: Request, res:Response) {
-    const newCategory = req.body.category;
-    const category = Category.create()
+    const {icon, name} = req.body;
+    const category = await Category.create({icon, name});
+
+    res.status(201).json(category);
 }
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,9 @@ mongoose.connect('mongodb://localhost:27017')
     .then(() => {
         const app = express();
 
+        app.use(express.json());
         app.use(appRouter);
-        
+
         app.listen(3001, () => {
             console.log('Server is running on port 3001');
         });
